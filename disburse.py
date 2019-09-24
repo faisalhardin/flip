4cd2e5cb0cb92cca36e33a0300970c8dd3d679c3
faisal hardin <faisalhardin23@gmail.com>
2019-09-24 10:19:44 +0700

    revise connection to db

--- disburse.py
+++ disburse.py
@@ -3,8 +3,8 @@ import slightlyBigFlipLibrary as flip_lib
 
 if __name__ == '__main__':
 
-    conn = flip_lib.create_connection()
-    create_table(conn)
+    conn = flip_lib.create_connection('./bank.db')
+    flip_lib.create_table(conn)
 
 
     if len(sys.argv) == 5:
@@ -12,9 +12,14 @@ if __name__ == '__main__':
         response = flip_lib.disburse(_id, status, receipt, time_served)
         if response:
             response['amount'] = int(response['amount'])
-            response['fee'] = int(reponse['fee'])
-            flip_lib.add_to_db(response)
+            response['fee'] = int(response['fee'])
+            response.pop('id')
+            db_response = flip_lib.add_to_db(conn, response)
+            print("db_response ", db_response)
         else:
             print("Problem with connection")
     else:
         print("Wrong number of arguments")
+
+    if conn:
+        conn.close()
