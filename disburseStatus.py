673874edb8f1e61e82ac121e30ca98c51c55eabc
faisal hardin <faisalhardin23@gmail.com>
2019-09-24 12:07:53 +0700

    connect to db

--- disburseStatus.py
+++ disburseStatus.py
@@ -2,12 +2,16 @@ import sys
 import slightlyBigFlipLibrary as flip_lib
 
 if __name__ == '__main__':
+    conn = flip_lib.create_connection('./bank.db')
+    flip_lib.create_table(conn)
+
     if len(sys.argv) == 2:
         _, _id = sys.argv
         response = flip_lib.disburse_status(_id)
         if response:
-            print(response)
+            # print(response)
             flip_lib.update_to_db(
+                conn,
                 response['id'],
                 response['status'],
                 response['receipt'],
