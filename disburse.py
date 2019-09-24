7127e4531f4731cf367a65f2a73a701d181c7364
faisal hardin <faisalhardin23@gmail.com>
2019-09-24 09:47:33 +0700

    fix disburse.py add_to_db()

--- disburse.py
+++ disburse.py
@@ -2,16 +2,18 @@ import sys
 import slightlyBigFlipLibrary as flip_lib
 
 if __name__ == '__main__':
+
+    conn = flip_lib.create_connection()
+    create_table(conn)
+
+
     if len(sys.argv) == 5:
         _, _id, status, receipt, time_served = sys.argv
         response = flip_lib.disburse(_id, status, receipt, time_served)
         if response:
-            flip_lib.add_to_db(
-                response['id'],
-                response['status'],
-                response['receipt'],
-                response['time_served']
-                )
+            response['amount'] = int(response['amount'])
+            response['fee'] = int(reponse['fee'])
+            flip_lib.add_to_db(response)
         else:
             print("Problem with connection")
     else:
